19edede3eb6051f12f6d7210eccb4a0837ce557f
SirGabriel22 <sirgabriel2208@gmail.com>
2025-02-27 23:24:39 -0300

    Primeira atualização.

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,11 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        //Casting:
+
+        short numero = 1;
+        int numeroInteiro = numero;
+        short outroNumero = (short) numeroInteiro;
+
+        System.out.println(outroNumero);
     }
 }
