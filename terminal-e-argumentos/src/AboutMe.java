12a2505ec0e992bd2f3d3ce59af97a0d30e0687a
SirGabriel22 <sirgabriel2208@gmail.com>
2025-03-03 17:57:57 -0300

    Linha de comando

---
+++ terminal-e-argumentos/src/AboutMe.java
@@ -0,0 +1,9 @@
+public class AboutMe {
+    public static void main(String[] args){
+        String nome = args[0];
+        int idade = args[1];
+        double altura = args[2];
+
+        System.out.println("Me chamo" + nome + ", tenho " + idade + " anos e tenho " + altura "m de altura");
+    }
+}
